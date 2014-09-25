9914516ff3426c577eb0d6b47a2773337fdea2bb
Nolan Lawson <nolan.lawson@gmail.com>
2014-09-24 22:36:00 -0400

    name the test

--- test/test.js
+++ test/test.js
@@ -14,11 +14,14 @@ chai.should(); // var should = chai.should();
 function tests() {
 
   describe('basic tests', function () {
-    var arr = [3, 2, 10, 12, 30, 25, 1, 4];
+    it('passes a smoke test', function () {
+      var arr = [3, 2, 10, 12, 30, 25, 1, 4];
 
-    sort(arr).should.deep.equal([ 1, 10, 12, 2, 25, 3, 30, 4 ]);
+      sort(arr).should.deep.equal([ 1, 10, 12, 2, 25, 3, 30, 4 ]);
 
-    sort(arr, function (a, b) { return a - b; }).should.deep.equal( [ 1, 2, 3, 4, 10, 12, 25, 30 ]);
+      sort(arr, function (a, b) { return a - b; }).should.deep.equal(
+        [ 1, 2, 3, 4, 10, 12, 25, 30 ]);
+    });
   });
 }
 
